8369c544f71c0e6ba890eee25815c8a6676820c6
echo <91651232+echothegoddess@users.noreply.github.com>
2021-11-20 20:49:13 +0330

    damn my code sucked

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import requests
 
 while 420>69:
-
     seetaken = (input("do you want to see taken accounts? (y/n):  "))
     if seetaken == "y" or "n":
         break
@@ -10,18 +9,13 @@ while 420>69:
         print("no, i said choose between y/n")
 
 while 3>2:
-    randomnumb = requests.get(f"https://random-word-api.herokuapp.com/word?number=1")
-    randomnumbstr = str(randomnumb.json())
-    x = (randomnumbstr.replace('"', '').replace("'", "").replace('[',"").replace('[',"").replace(']',""))
+    x= str(requests.get(f"https://random-word-api.herokuapp.com/word?number=1")).replace('"', '').replace("'", "").replace('[',"").replace(']',"")
 
     
-    resp = requests.get(f"https://api.ashcon.app/mojang/v2/user/{x}")
-    jsoo = str(resp.json())
+    jsoo = str(requests.get(f"https://api.ashcon.app/mojang/v2/user/{x}").json())
     if jsoo.startswith("{'code': 404,") == True:
         print(f"{x} - account available")
-    elif jsoo.startswith("{'code': 400,") == True:
-        if seetaken == "y":
-            print(f"ok what the fuck, this account isnt supposed to exist")
-    elif jsoo.startswith("{'uuid':") == True:
-        if seetaken == "y":
+    elif jsoo.startswith("{'code': 400,") == True and seetaken == "y":
+            print("ok what the fuck, this account isnt supposed to exist")
+    elif jsoo.startswith("{'uuid':") == True and seetaken == "y":
             print(f"{x} - account taken")
